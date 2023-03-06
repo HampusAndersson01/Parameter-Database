665192535e97ca49b3ffbb612b845fcd7dd43ef4
HampusAndersson01 <hampe.01@hotmail.com>
2023-03-06 10:58:49 +0100

    Actually adding build

---
+++ build/precache-manifest.1e3e07dbb22a79851dfb4c038a06a428.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5430899601bb5e718ef6",
+    "url": "/parameter_db/static/css/main.f407a533.chunk.css"
+  },
+  {
+    "revision": "5430899601bb5e718ef6",
+    "url": "/parameter_db/static/js/main.ee5b8066.chunk.js"
+  },
+  {
+    "revision": "8ce3b6f87d63bfbf0389",
+    "url": "/parameter_db/static/js/runtime~main.95f84dd8.js"
+  },
+  {
+    "revision": "55bb86271be72104e66a",
+    "url": "/parameter_db/static/js/2.7cd62210.chunk.js"
+  },
+  {
+    "revision": "ea7ded9f1f5094e14392",
+    "url": "/parameter_db/static/js/3.d433891d.chunk.js"
+  },
+  {
+    "revision": "c42374cb57337736b9c9c89cd2838f99",
+    "url": "/parameter_db/index.html"
+  }
+];
